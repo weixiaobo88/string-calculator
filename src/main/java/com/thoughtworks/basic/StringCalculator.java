5d67288d38fbcf6a948e2b5b389da69104dbd954
Zhaoyu <weixiaobo88@gmail.com>
2020-08-19 11:12:52 +0800

    [Linne] - refactor: extract generateSplitStrings method

--- src/main/java/com/thoughtworks/basic/StringCalculator.java
+++ src/main/java/com/thoughtworks/basic/StringCalculator.java
@@ -9,7 +9,6 @@ class StringCalculator {
         String NUMBER_SEPARATOR = ",";
         String LINE_SEPARATOR = "\n";
         String separators = "[" + NUMBER_SEPARATOR + LINE_SEPARATOR + "]";
-        String[] splitString;
 
         if (string.isEmpty()) {
             return 0;
@@ -19,26 +18,37 @@ class StringCalculator {
             return parseToInt(string);
         }
 
-        if (string.startsWith(PREFIX_OF_CHANGE_SEPARATOR)) {
-            int separatorBeginIndex = 2;
-            int separatorEndIndex = 3;
-            int validStringBeginIndex = 4;
-            String separator = string.substring(separatorBeginIndex, separatorEndIndex);
-            splitString = splitBySeparators(string.substring(validStringBeginIndex), separator);
-        } else {
-            splitString = splitBySeparators(string, separators);
-        }
-
-        return Arrays.stream(splitString)
+        return Arrays.stream(generateSplitStrings(string, separators))
                 .mapToInt(this::parseToInt)
                 .sum();
+    }
+
+    private int parseToInt(String string) {
+        return Integer.parseInt(string);
+    }
+
+    private String[] generateSplitStrings(String string, String separators) {
+        if (string.startsWith(PREFIX_OF_CHANGE_SEPARATOR)) {
+            return splitBySeparators(pickValidString(string), pickSeparator(string));
+        }
+
+        return splitBySeparators(string, separators);
     }
 
     private String[] splitBySeparators(String string, String separators) {
         return string.split(separators);
     }
 
-    private int parseToInt(String string) {
-        return Integer.parseInt(string);
+    private String pickValidString(String string) {
+        int validStringBeginIndex = 4;
+
+        return string.substring(validStringBeginIndex);
+    }
+
+    private String pickSeparator(String string) {
+        int separatorBeginIndex = 2;
+        int separatorEndIndex = 3;
+
+        return string.substring(separatorBeginIndex, separatorEndIndex);
     }
 }
